aca09f0ea2c16f9902e487d6bc364ffa95ea4b7c
Larbi <ferhaoui.20044@gmail.com>
2020-11-10 17:52:49 +0100

    ADD page product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+// pages
 import Home from './Pages/Home';
 import Products from './Pages/Products';
 import Cart from './Pages/Cart';
+import Product from './Pages/Product';
 
 class App extends Component {
   render() {
@@ -12,7 +14,8 @@ class App extends Component {
         <div className="container">
           <Navbar />
           <Route path="/" exact component={Home} />
-          <Route path="/products" exut component={Products} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={Product} />
           <Route path="/cart" component={Cart} />
         </div>
       </Router>
