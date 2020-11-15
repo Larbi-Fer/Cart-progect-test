b046ae3dff467ce9b4552c1a8559da8abfd8f630
Larbi <ferhaoui.20044@gmail.com>
2020-11-15 19:07:44 +0100

    Sync to local storage

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,35 +1,33 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import cartReduer from './reducers'
 import thunk from 'redux-thunk';
+import throttle from 'lodash.throttle'
 
-const initialState = {
-    cart: [
-        {
-            product: {
-                "id": 1,
-                "name": "mobile",
-                "price": 200,
-                "image": "/img/mobile1.jpg",
-                "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident necessitatibus recusandae debitis omnis cupiditate similique nesciunt ullam at! Eligendi officiis quia commodi aut natus assumenda vel molestias, voluptate illo modi."
-            },
-            quantity: 2
-        },
-        {
-            product: {
-                "id": 2,
-                "name": "ios",
-                "price": 250,
-                "image": "/img/mobile2.jpg",
-                "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident necessitatibus recusandae debitis omnis cupiditate similique nesciunt ullam at! Eligendi officiis quia commodi aut natus assumenda vel molestias, voluptate illo modi."
-            },
-            quantity: 1
-        }
-    ]
+function localState() {
+
+    const state = localStorage.getItem('cart')
+
+    if ( state != null ) {
+        return JSON.parse(state)
+    }
+
+    return {
+        cart: []
+    }
 }
 
-const store = createStore(cartReduer, initialState, compose(
+function saveState(state) {
+    localStorage.setItem('cart',JSON.stringify(state) )
+}
+
+
+const store = createStore(cartReduer, localState(), compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ) )
+
+store.subscribe(throttle(() => {
+    saveState(store.getState())
+}, 2000))
 
 export default store
